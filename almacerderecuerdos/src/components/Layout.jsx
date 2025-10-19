fc261af1697c59798de607e350a3f7cce4402c29
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-19 23:47:37 +0200

    Update Layout.jsx

--- almacerderecuerdos/src/components/Layout.jsx
+++ almacerderecuerdos/src/components/Layout.jsx
@@ -12,7 +12,7 @@ export default function Layout() {
             <main>
                 <Outlet />
             </main>
-        <Footer />
+        {/* <Footer /> */}
     </>            
     );
 }
