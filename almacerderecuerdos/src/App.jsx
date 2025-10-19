18bc53cba7c1b3ed4237058174850dc5106148ff
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-19 15:42:29 +0200

    Update App.jsx

--- almacerderecuerdos/src/App.jsx
+++ almacerderecuerdos/src/App.jsx
@@ -1,20 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 
-import Layout from "./Components/Layout";
-import Home from "./Pages/Home";
-import Proyectos from "./Pages/Proyectos";
-import SobreMi from "./Pages/SobreMi";
-import Estados from "./Pages/Estados";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Proyectos from "./pages/Proyectos";
+import SobreMi from "./pages/SobreMi";
+import Estados from "./pages/Estados";
 //import './App.css'
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="./Home" element={<Home />} />
-        <Route path="./Proyectos" element={<Proyectos />} />
-        <Route path="./Sobre-mi" element={<SobreMi />} />
-        <Route path="./Estados" element={<Estados />} />
+        <Route path="./home" element={<Home />} />
+        <Route path="./proyectos" element={<Proyectos />} />
+        <Route path="./pobre-mi" element={<SobreMi />} />
+        <Route path="./estados" element={<Estados />} />
       </Route>
     </Routes>
   );
