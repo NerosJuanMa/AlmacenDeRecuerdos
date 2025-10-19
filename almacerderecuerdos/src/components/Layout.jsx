c0746b498586ff2fa84a3ce4d01cce7272124e44
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-19 23:43:44 +0200

    Create Layout.jsx

---
+++ almacerderecuerdos/src/components/Layout.jsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+
+
+
+export default function Layout() {
+    return (
+    <>
+        <Header />
+        {/* <Nav /> */}
+        {/* <Sidebar /> */}
+            <main>
+                <Outlet />
+            </main>
+        <Footer />
+    </>            
+    );
+}
