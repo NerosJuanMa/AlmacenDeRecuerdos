4e5d410c39ddb8f0e57ba72b56ec669e31574c02
Neros__JuanMa <jmmudarra@gmail.com>
2025-10-28 07:55:14 +0100

    Update Home.jsx

--- almacerderecuerdos/src/pages/Home.jsx
+++ almacerderecuerdos/src/pages/Home.jsx
@@ -36,6 +36,42 @@ export default function Home() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+          <div className='contenedor-imagen'>
+      <img src={TATOO_NEROS_V2_ROJO2} alt="fondo" />    
+    </div>
+    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus error fugit cum, omnis officia deleniti, praesentium commodi repellendus quis, dolorum alias magnam molestiae inventore officiis ratione quibusdam temporibus blanditiis.</p><br /><br /><br /><br /><br />
+    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus error fugit cum, omnis officia deleniti, praesentium commodi repellendus quis, dolorum alias magnam molestiae inventore officiis ratione quibusdam temporibus blanditiis.</p><br /><br /><br /><br /><br />
+    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus error fugit cum, omnis officia deleniti, praesentium commodi repellendus quis, dolorum alias magnam molestiae inventore officiis ratione quibusdam temporibus blanditiis.</p><br /><br /><br /><br /><br />
+    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus error fugit cum, omnis officia deleniti, praesentium commodi repellendus quis, dolorum alias magnam molestiae inventore officiis ratione quibusdam temporibus blanditiis.</p><br /><br /><br /><br /><br />
+    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus error fugit cum, omnis officia deleniti, praesentium commodi repellendus quis, dolorum alias magnam molestiae inventore officiis ratione quibusdam temporibus blanditiis.</p><br /><br /><br /><br /><br />
+    <br /><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure delectus error fugit cum, omnis officia deleniti, praesentium commodi repellendus quis, dolorum alias magnam molestiae inventore officiis ratione quibusdam temporibus blanditiis.</p><br /><br /><br /><br /><br />
+     <div className='home'>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    
     </>
   )
 }
+
+
+
+
+    
